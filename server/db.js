ba4cbadf25c531370d0d444e080e05ac3cb4d66e
hardik6869 <sorthiyahardik6869@gmail.com>
2024-04-22 10:45:20 +0530

    feat: Add hostname to environment file

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
-const { PG_USER, PASSWORD, PG_PORT, PG_DATABASE } = process.env;
+const { PG_USER, PASSWORD, PG_PORT, PG_DATABASE, PG_HOST } = process.env;
 
 const pool = new Pool({
   user: PG_USER,
   password: PASSWORD,
-  host: "localhost",
+  host: PG_HOST,
   port: PG_PORT,
   database: PG_DATABASE,
 });
